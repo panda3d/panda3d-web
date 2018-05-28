810883b75233471feaa0adce05c3cb8266d6c5dd
Joey Z <joey.r.ziolkowski@gmail.com>
2018-05-28 15:33:00 -0400

    git: Update gitignore to remove node modules and compiled assets

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<?php if ( is_home() && ! is_front_page() ) { ?>
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+	<?php } ?>
 
 	<?php panda3d_post_thumbnail(); ?>
 
