5b3c2ef036e5d4d1f56fe4049d3ce83ec956653e
Joey Z <joey.r.ziolkowski@gmail.com>
2018-05-31 05:18:09 -0400

    acf: Implement and style Blog Posts flexible component

--- inc/acf/flexible-components/blog-posts.php
+++ inc/acf/flexible-components/blog-posts.php
@@ -1,55 +1,52 @@
 <?php
-     $blog_category = get_sub_field('blog_category');
+    $blog_category = get_sub_field('blog_category');
 
-     $args = array(
-          'post_type'      => 'post',
-          'category_name'  => $blog_category,
-          'posts_per_page' => 6
-     );
+    $args = array(
+        'post_type'      => 'post',
+        'category_name'  => $blog_category,
+        'posts_per_page' => 3
+    );
 
-     $loop = new WP_Query( $args );
+    $loop = new WP_Query( $args );
 ?>
 
-     <section class="blog-posts">
-          <div class="wrap">
+<section class="blog-posts">
+    <div class="wrap">
 
-               <div class="blog-section-header">
-                   <h1><?php the_sub_field('header'); ?></h1>
-               </div>
+        <div class="blog-section-header">
+            <h1><?php the_sub_field('header'); ?></h1>
 
-               <div class="article__container">
-                    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                    <?php if ( has_post_thumbnail() ) : ?>
-                        <div class="article" style="background: url('<?php the_post_thumbnail_url('large'); ?>')">
-                    <?php else: ?>
-                        <div class="article">
-                    <?php endif; ?>
-                    <div class="inner">
-                               <a href="<?php the_permalink(); ?>">
-                                   <div class="article__info">
-                                        <div class="article__title">
-                                             <h3><?php the_title(); ?></h3>
-                                        </div>
-                                        <div class="article__details">
-                                             <span><?php the_author(); ?> - <i><?php the_date('M j Y'); ?></i></span>
-                                        </div>
-                                   </div>
-                                </a>
-                    </div>
-                        </div>
-                    <?php endwhile; ?>
-               </div>
-
-               <div class="blog-posts__more">
-                    <?php if ($news_category === '') { ?>
-                         <a href="/news/" class="cta--action">All Posts</a>
-                    <?php } else { ?>
+            <div class="blog-posts__more">
+                <?php if ($news_category === '') { ?>
+                    <a href="/news/" class="cta--action">All Posts</a>
+                <?php } else { ?>
                     <a href="/category/<?php echo $selected_category; ?>" class="cta--action">View More News</a>
-                    <?php } ?>
-               </div>
+                <?php } ?>
+            </div>
+        </div>
 
-          </div>
-     </section>
+        <div class="article__container">
 
-	<?php
-          wp_reset_postdata();
+            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                <div class="article">
+                    <a href="<?php the_permalink(); ?>">
+                        <div class="article__image" style="background-image: url('<?php the_post_thumbnail_url('large'); ?>');"></div>
+                        <div class="article__info">
+                            <div class="article__title">
+                                <h3><?php the_title(); ?></h3>
+                            </div>
+                            <div class="article__details">
+                                <span><?php the_author(); ?> - <i><?php the_date('M j Y'); ?></i></span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php endwhile; ?>
+
+        </div>
+
+    </div>
+</section>
+
+<?php
+wp_reset_postdata();
