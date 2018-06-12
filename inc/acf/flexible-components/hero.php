5299bfc5e06ae4ab66976bd10fdaa83101b9fb48
Joey Z <joey.r.ziolkowski@gmail.com>
2018-06-12 16:29:10 -0400

    elements: Add icon support for Call to Action buttons

--- inc/acf/flexible-components/hero.php
+++ inc/acf/flexible-components/hero.php
@@ -10,12 +10,14 @@
             <div class="hero__calltoaction">
                 <?php if(get_field('primary_call_to_action', 'option')) { ?>
                     <a href="<?php the_field('primary_call_to_action_url', 'option'); ?>" class="cta--primary">
+                        <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
                         <?php the_field('primary_call_to_action_text', 'option'); ?>
                     </a>
                 <?php } ?>
 
                 <?php if(get_field('secondary_call_to_action', 'option')) { ?>
                     <a href="<?php the_field('secondary_call_to_action_url', 'option'); ?>" class="cta--secondary">
+                        <i class="fas fa-<?php the_field('secondary_call_to_action_icon', 'option'); ?>"></i>
                         <?php the_field('secondary_call_to_action_text', 'option'); ?>
                     </a>
                 <?php } ?>
