b9b66e7dd1c971d250c773abb24bf975496bd761
Joey Z <joey.r.ziolkowski@gmail.com>
2018-06-12 16:22:00 -0400

    js: Drop in FontAwesome 5

--- inc/theme/enqueue-scripts.php
+++ inc/theme/enqueue-scripts.php
@@ -4,17 +4,18 @@
  */
 function panda3d_scripts() {
 	// Vendor
-	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '2018518', true );
+	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '20180518', true );
+	wp_enqueue_script( 'fontawesome', get_template_directory_uri() . '/assets/js/vendor/fontawesome-all.min.js', array(), '20180612', true );
 
 	// Theme
 	wp_enqueue_style( 'panda3d-style', get_stylesheet_uri() );
-	wp_enqueue_script( 'panda3d-js', get_template_directory_uri() . '/bundle.min.js', array(), '2018518', true );
+	wp_enqueue_script( 'panda3d-js', get_template_directory_uri() . '/bundle.min.js', array(), '20180518', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	// Fonts
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:100,400,700', array(), '2018518', true );
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:100,400,700', array(), '20180518', true );
 }
 add_action( 'wp_enqueue_scripts', 'panda3d_scripts' );
