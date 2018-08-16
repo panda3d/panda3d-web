0da354ac00e9de0479b1f61cc6aae974aaafaa96
Joey Z <joey.r.ziolkowski@gmail.com>
2018-08-16 15:57:50 -0400

    acf: Implement custom icon support for the Call to Action component

--- inc/acf/flexible-components/call-to-action.php
+++ inc/acf/flexible-components/call-to-action.php
@@ -2,19 +2,21 @@
     <div class="call-to-action__container">
 
         <div class="call-to-action__text">
-            <h1><?php the_sub_field('header') ?></h1>
-            <p class="subheader"><?php the_sub_field('description') ?></p>
+            <h1><?php the_sub_field('header'); ?></h1>
+            <p class="subheader"><?php the_sub_field('description'); ?></p>
 
             <div class="call-to-action__buttons">
                 <?php if(get_sub_field('display_secondary_button')) { ?>
                     <?php $secondary_button = get_sub_field('custom_secondary_button'); ?>
                     <?php if($secondary_button['override']) { ?>
                         <a href="<?php echo $secondary_button['url']; ?>" class="cta--secondary">
+                            <i class="fas fa-<?php echo $secondary_button['icon']; ?>"></i>
                             <?php echo $secondary_button['label']; ?>
                         </a>
                     <?php } else { ?>
                         <?php if(get_field('secondary_call_to_action', 'option')) { ?>
                             <a href="<?php the_field('secondary_call_to_action_url', 'option'); ?>" class="cta--secondary">
+                                <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
                                 <?php the_field('secondary_call_to_action_text', 'option'); ?>
                             </a>
                         <?php } ?>
@@ -25,11 +27,13 @@
                     <?php $primary_button = get_sub_field('custom_primary_button'); ?>
                     <?php if($primary_button['override']) { ?>
                         <a href="<?php echo $primary_button['url']; ?>" class="cta--primary">
+                            <i class="fas fa-<?php echo $primary_button['icon']; ?>"></i>
                             <?php echo $primary_button['label']; ?>
                         </a>
                     <?php } else { ?>
                         <?php if(get_field('primary_call_to_action', 'option')) { ?>
                             <a href="<?php the_field('primary_call_to_action_url', 'option'); ?>" class="cta--primary">
+                                <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
                                 <?php the_field('primary_call_to_action_text', 'option'); ?>
                             </a>
                         <?php } ?>
@@ -40,7 +44,7 @@
         </div>
 
         <div class="call-to-action__icon">
-            <i class="far fa-box-open fa-10x"></i>
+            <i class="far fa-<?php the_sub_field('icon'); ?> fa-10x"></i>
         </div>
 
     </div>
