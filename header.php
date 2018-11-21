52d33d58a0c2583b377b17fccfef16dcf53a5fc4
Joey Z <joey.r.ziolkowski@gmail.com>
2018-11-21 03:30:50 -0500

    layout: Add mobile menu icon to the header

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'panda3d' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i title="<?php esc_html_e( 'Menu', 'panda3d' ); ?>" class="fas fa-bars"></i></button>
 				<?php
 				wp_nav_menu( array(
 					'theme_location' => 'primary-menu',
