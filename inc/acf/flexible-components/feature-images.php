3fcdcb357cb5e244380a1623843cf88cd20e1df4
Joey Z <joey.r.ziolkowski@gmail.com>
2018-12-05 21:35:54 -0500

    acf: Fix up a few spacing issues with flexible components

--- inc/acf/flexible-components/feature-images.php
+++ inc/acf/flexible-components/feature-images.php
@@ -1,8 +1,10 @@
 <section class="feature-images">
     <div class="feature-images__outer">
 
-        <h2><?php the_sub_field('header') ?></h2>
-        <p class="subheader"><?php the_sub_field('subheader') ?></p>
+        <div class="wrap">
+            <h2><?php the_sub_field('header') ?></h2>
+            <p class="subheader"><?php the_sub_field('subheader') ?></p>
+        </div>
 
         <div class="feature-images__inner">
             <?php if( have_rows('feature_images') ): while ( have_rows('feature_images') ) : the_row(); ?>
