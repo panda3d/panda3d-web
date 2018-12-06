0e8324324b05998246f3aa30f8ce40acb50ba972
Joey Z <joey.r.ziolkowski@gmail.com>
2018-12-05 21:48:11 -0500

    acf: Put a tighter wrap around the feature images component

--- inc/acf/flexible-components/feature-images.php
+++ inc/acf/flexible-components/feature-images.php
@@ -1,10 +1,8 @@
 <section class="feature-images">
     <div class="feature-images__outer">
 
-        <div class="wrap">
-            <h2><?php the_sub_field('header') ?></h2>
-            <p class="subheader"><?php the_sub_field('subheader') ?></p>
-        </div>
+        <h2><?php the_sub_field('header') ?></h2>
+        <p class="subheader"><?php the_sub_field('subheader') ?></p>
 
         <div class="feature-images__inner">
             <?php if( have_rows('feature_images') ): while ( have_rows('feature_images') ) : the_row(); ?>
