b72f685b22029dc6fe06f1b706c1a3d826f6606d
Joey Z <joey.r.ziolkowski@gmail.com>
2018-12-29 17:09:57 -0500

    acf: Begin setting up framework for a custom downloads page for the SDK

---
+++ download-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Download Page
+ * Description: Displays Downloads set up on the "P3D Downloads" page from the admin panel.
+ *
+ * @package Panda3D
+ */
+
+get_header();
+?>
+
+<div class="entry-content">
+
+</div>
+
+<?php
+get_footer();
--- inc/acf/acf-options.php
+++ inc/acf/acf-options.php
@@ -6,9 +6,16 @@
  */
 
 if( function_exists('acf_add_options_page') ) {
+    acf_add_options_page(array(
+		'page_title' 	=> 'Panda3D Download Management',
+		'menu_title'	=> 'P3D Downloads',
+        'menu_slug' 	=> 'panda3d-downloads',
+        'icon_url'      => 'dashicons-download',
+	));
+
 	acf_add_options_page(array(
 		'page_title' 	=> 'Panda3D Theme Settings',
-		'menu_title'	=> 'Theme Settings',
+		'menu_title'	=> 'P3D Theme',
 		'menu_slug' 	=> 'panda3d-theme-settings',
-	));
+    ));
 }
