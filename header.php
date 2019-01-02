36fa7c091d6600eac59e0c671c8e4190a1cdb976
Joey Z <joey.r.ziolkowski@gmail.com>
2019-01-01 19:46:28 -0500

    head: Add theme color meta tag

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="theme-color" content="#323031">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
