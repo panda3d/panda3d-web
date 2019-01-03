d52acac7b4ac5e58b1091e79b200b8ce90566cfc
Joey Z <joey.r.ziolkowski@gmail.com>
2019-01-03 17:45:19 -0500

    acf: Add latest download version to components with a primary call to action

--- inc/acf/flexible-components/call-to-action-alt.php
+++ inc/acf/flexible-components/call-to-action-alt.php
@@ -18,10 +18,25 @@
                         <?php echo $primary_button['label']; ?>
                     </a>
                 <?php } else { ?>
+                    <?php
+                    // Get info about latest SDK version for the Call to Action
+                    $latest_download = wp_get_recent_posts(array(
+                        'numberposts' => 1,
+                        'post_type' => 'download',
+                        'post_status' => 'publish'
+                    ))[0];
+
+                    $latest_id = $latest_download['ID'];
+                    $latest_version = $latest_download['post_title'];
+                    ?>
+
                     <?php if(get_field('primary_call_to_action', 'option')) { ?>
-                        <a href="<?php the_field('primary_call_to_action_url', 'option'); ?>" class="cta cta--primary">
-                            <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
-                            <?php the_field('primary_call_to_action_text', 'option'); ?>
+                        <a class="cta cta--primary-ver" href="<?php echo get_permalink($latest_id); ?>">
+                            <span class="cta-ver"><?php echo $latest_version; ?></span>
+                            <span class="cta-text">
+                                <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
+                                <?php the_field('primary_call_to_action_text', 'option'); ?>
+                            </span>
                         </a>
                     <?php } ?>
                 <?php } ?>
--- inc/acf/flexible-components/hero.php
+++ inc/acf/flexible-components/hero.php
@@ -1,3 +1,15 @@
+<?php
+// Get info about latest SDK version for the Call to Action
+$latest_download = wp_get_recent_posts(array(
+	'numberposts' => 1,
+    'post_type' => 'download',
+    'post_status' => 'publish'
+))[0];
+
+$latest_id = $latest_download['ID'];
+$latest_version = $latest_download['post_title'];
+?>
+
 <section class="hero">
     <div class="background-image" style="background-image: url(<?php the_sub_field( 'background_image' ) ?>);"></div>
     <div class="hero__wrap">
@@ -7,9 +19,12 @@
 
             <div class="hero__calltoaction">
                 <?php if(get_field('primary_call_to_action', 'option')) { ?>
-                    <a href="<?php the_field('primary_call_to_action_url', 'option'); ?>" class="cta cta--primary">
-                        <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
-                        <?php the_field('primary_call_to_action_text', 'option'); ?>
+                    <a class="cta cta--primary-ver" href="<?php echo get_permalink($latest_id); ?>">
+                        <span class="cta-ver"><?php echo $latest_version; ?></span>
+                        <span class="cta-text">
+                            <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
+                            <?php the_field('primary_call_to_action_text', 'option'); ?>
+                        </span>
                     </a>
                 <?php } ?>
 
