11b60b17b4363364fc8490c46bc806ed85d5bc03
Joey Z <joey.r.ziolkowski@gmail.com>
2019-01-02 20:57:53 -0500

    acf: Reduce size of buttons on the choices component

--- inc/acf/flexible-components/choices.php
+++ inc/acf/flexible-components/choices.php
@@ -8,7 +8,7 @@
 
         <div class="choices__buttons">
             <?php if( have_rows('link_choices') ): while ( have_rows('link_choices') ) : the_row(); ?>
-                <a class="cta cta--secondary" href="<?php the_sub_field('url') ?>"><i class="fas fa-<?php the_sub_field('icon') ?>"></i><?php the_sub_field('label') ?></a>
+                <a class="cta cta--secondary" href="<?php the_sub_field('url') ?>"><i class="fas fa-<?php the_sub_field('icon') ?>"></i> <?php the_sub_field('label') ?></a>
             <?php endwhile; endif; ?>
         </div>
 
