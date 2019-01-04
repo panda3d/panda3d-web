d1b527f93cc50acbe3815fe02196cf6aece282d3
Joey Z <joey.r.ziolkowski@gmail.com>
2019-01-04 02:43:02 -0500

    templates: Add widget area support for single posts

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all single posts.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
@@ -17,7 +17,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part('template-parts/content', get_post_type());
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -27,9 +27,8 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php
-get_sidebar();
 get_footer();
--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -28,27 +28,33 @@
         </div>
 	</header>
 
-	<div class="entry-content block">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'panda3d' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+	<div class="entry-content">
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'panda3d' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+        <div class="block">
+            <?php
+            the_content( sprintf(
+                wp_kses(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                    __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'panda3d' ),
+                    array(
+                        'span' => array(
+                            'class' => array(),
+                        ),
+                    )
+                ),
+                get_the_title()
+            ) );
+
+            wp_link_pages( array(
+                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'panda3d' ),
+                'after'  => '</div>',
+            ) );
+            ?>
+        </div>
+
+        <?php get_sidebar(); ?>
+
+    </div>
 
 	<!-- <footer class="entry-footer">
         <?php
