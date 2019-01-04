27df25c451ba2e5dea4c8027f09c8d4d67691ade
Joey Z <joey.r.ziolkowski@gmail.com>
2019-01-03 19:11:27 -0500

    acf: Remove deprecated Call to Action URL field

--- inc/acf/flexible-components/call-to-action.php
+++ inc/acf/flexible-components/call-to-action.php
@@ -32,9 +32,24 @@
                         </a>
                     <?php } else { ?>
                         <?php if(get_field('primary_call_to_action', 'option')) { ?>
-                            <a href="<?php the_field('primary_call_to_action_url', 'option'); ?>" class="cta cta--primary">
-                                <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
-                                <?php the_field('primary_call_to_action_text', 'option'); ?>
+                            <?php
+                            // Get info about latest SDK version for the Call to Action
+                            $latest_download = wp_get_recent_posts(array(
+                                'numberposts' => 1,
+                                'post_type' => 'download',
+                                'post_status' => 'publish'
+                            ))[0];
+
+                            $latest_id = $latest_download['ID'];
+                            $latest_version = $latest_download['post_title'];
+                            ?>
+
+                            <a class="cta cta--primary-ver" href="<?php echo get_permalink($latest_id); ?>">
+                                <span class="cta-ver"><?php echo $latest_version; ?></span>
+                                <span class="cta-text">
+                                    <i class="fas fa-<?php the_field('primary_call_to_action_icon', 'option'); ?>"></i>
+                                    <?php the_field('primary_call_to_action_text', 'option'); ?>
+                                </span>
                             </a>
                         <?php } ?>
                     <?php } ?>
