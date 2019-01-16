2d66ed2a11dc6a2ab440ad1c2d3c30882e69d458
Joey Z <joey.r.ziolkowski@gmail.com>
2019-01-16 02:27:47 -0500

    core: Fix a bug hiding runtime downloads on the admin dashboard

--- inc/core/query-modifications.php
+++ inc/core/query-modifications.php
@@ -7,7 +7,7 @@
 
 function panda3d_pre_get_posts($query) {
     // Only display SDK downloads on the download archive page
-    if(is_post_type_archive('download') && $query->is_main_query()) {
+    if(is_post_type_archive('download') && !is_admin() && $query->is_main_query()) {
          $query->set('tax_query', array(
             array(
                 'taxonomy' => 'download_category',
